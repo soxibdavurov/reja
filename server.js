f9755a128231b1bf01ae6757b6316f6bfcdb913d
Sokhibjon <soxib.davurov@gmail.com>
2025-05-31 00:49:49 +0900

    xarid

--- server.js
+++ server.js
@@ -14,12 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 app.set("views", "views"); 
 app.set("view engine", "ejs");
 // 4 Routing code 
-app.get("/hello", function (req, res) { 
-    res.end(`<h1>HELLO WORLD</h1>`); 
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test:"success"});
 });
 
-app.get("/gift", function (req, res) { 
-    res.end(` <h1>Siz sovg'alar sahifasidasiz</h1>`); 
+app.get("/", function(req, res) {
+    res.render("xarid");
 });
 
     const server = http.createServer(app); 
