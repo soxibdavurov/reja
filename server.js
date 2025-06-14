b629535e526a2344f6ed26f7e9422f7ad3ab5e6d
Sokhibjon <soxib.davurov@gmail.com>
2025-06-15 01:47:13 +0900

    PORT updated to 3006

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongodb.connect(connectionString, {
 
     const app = require("./app");
     const server = http.createServer(app); 
-    let PORT = 3000; 
+    let PORT = 3006; 
     server.listen(PORT, function () { 
         console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
     });
